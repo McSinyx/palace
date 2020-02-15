d15649dad0aad32d5a4755d29a954ad2958e1cf1
Nguyễn Gia Phong <vn.mcsinyx@gmail.com>
2020-02-15 14:42:38 +0700

    Build documentation for version 0.0.9

--- src/conf.py
+++ src/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, 2020  Nguyễn Gia Phong & contributors'
 author = 'Nguyễn Gia Phong & contributors'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
