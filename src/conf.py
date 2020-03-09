b40cd800b4c5fa3da424c84e628b134c71a6fb0e
Nguyễn Gia Phong <mcsinyx@disroot.org>
2020-03-09 17:39:23 +0700

    Update reference doc

--- src/conf.py
+++ src/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, 2020  Nguyễn Gia Phong et al'
 author = 'Nguyễn Gia Phong et al.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.11'
+release = '0.0.12'
 
 
 # -- General configuration ---------------------------------------------------
