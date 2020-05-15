4caaee5727cf8962fbd23b76711c5b3b9049b81d
Ngô Ngọc Đức Huy <duchuy29092000@gmail.com>
2020-05-15 10:42:50 +0700

    Changing layout for for palace
    Make the sidebar fixed and add prev/next navigation

--- src/conf.py
+++ src/conf.py
@@ -58,6 +58,7 @@ html_sidebars = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = {'fixed_sidebar': True, 'show_relbars': True}
 
 
 # -- Options for LaTeX output ------------------------------------------------
